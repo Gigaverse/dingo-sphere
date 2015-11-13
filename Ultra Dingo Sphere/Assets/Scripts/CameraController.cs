8cd900fdc19659f2a5cf3ef8b1739b9264988978
Youssef Mahmoud <youssefpower1997@live.com>
2015-11-13 15:10:08 -0600

    Initial
    Added a ball and platform, and made the camera follow it
    
    TODO:
    - make the camera not reset back to its original position

---
+++ Ultra Dingo Sphere/Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	
+	public GameObject target;
+	public float rotateSpeed = 1;
+	public float sensitivity;
+	Vector3 offset;
+	
+	void Start() {
+		offset = target.transform.position - transform.position;
+	}
+	
+	void LateUpdate() {
+		float horizontal = Input.GetAxis("Right Stick X") * rotateSpeed;
+		//target.transform.Rotate(0, horizontal, 0);
+
+		Quaternion rotation = Quaternion.Euler(0, horizontal*sensitivity, 0);
+		transform.position = target.transform.position - (rotation*offset);
+		
+		transform.LookAt(target.transform);
+	}
+}
